45c9a26e30a05a1bf8f6322c79a71a87aeb3158e
Sakshi Shreya <shreyasakshi96@gmail.com>
2024-09-28 19:58:48 +0530

    12: Add rate limiting

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import compression from "compression";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 import ingredients from "./routes/ingredients.js";
 import globalErrorHandler from "./utils/globalErrorHandler.js";
 
@@ -15,10 +16,21 @@ app.use(morgan("dev"));
 // 2. CORS issue
 app.use(cors());
 
-// 3. Get body
+// 3. Rate limiting
+app.use(
+  rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 min
+    max: 20,
+    message: { msg: "You are rate limited! Please try after 10 min." },
+    standardHeaders: true,
+    legacyHeaders: false
+  })
+);
+
+// 4. Get body
 app.use(express.json());
 
-// 4. Compress api response
+// 5. Compress api response
 app.use(compression());
 
 // 6. Routes
