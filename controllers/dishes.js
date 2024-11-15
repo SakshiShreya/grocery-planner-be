87637a96b2a92b73b25e80272f17b99dea1439ec
Soumya-Shreya <shreya201197@gmail.com>
2024-11-14 21:31:57 -0800

    Fixing populate for ingredients

--- controllers/dishes.js
+++ controllers/dishes.js
@@ -5,7 +5,7 @@ export async function getAllDishes(req, res, next) {
     const { page = 1, limit = 10, q = "" } = req.query;
     const filter = { name: { $regex: q, $options: "i" } };
     const findPromise = Dishes.find(filter, {}, { skip: limit * (page - 1), limit })
-      .populate("ingredients", "ingredient")
+      .populate("ingredients.ingredient")
       .populate("createdBy", "name fName lName")
       .populate("updatedBy", "name fName lName");
     const countPromise = Dishes.countDocuments(filter);
