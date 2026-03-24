7e7ecaa1b3dbf9077db2e2d00d30a013f76e04e3
SakshiShreya <shreyasakshi96@gmail.com>
2026-03-25 01:50:29 +0530

    54: Update a meal

--- controllers/plans.js
+++ controllers/plans.js
@@ -56,34 +56,36 @@ export async function createPlan(req, res, next) {
   }
 }
 
-export async function deletePlan(req, res, next) {
-  try {
-    const { id } = req.params;
-
-    await Plans.findByIdAndDelete(id);
-
-    res.status(204).json();
-  } catch (error) {
-    next(error);
-  }
-}
-
-export async function createMeal(req, res, next) {
+export async function updateMeal(req, res, next) {
   try {
     const { id } = req.params;
     const { body, user } = req;
+    const { day, mealType, dishes } = body;
 
-    const planBody = {
-      updatedBy: user._id,
-      $push: { meals: body },
-    };
+    // Try to update existing meal with matching day + mealType
+    const updatedPlan = await Plans.findOneAndUpdate(
+      { _id: id, "meals.day": day, "meals.mealType": mealType },
+      {
+        updatedBy: user._id,
+        $set: { "meals.$.dishes": dishes },
+      },
+      { new: true, runValidators: true }
+    );
 
-    const plan = await Plans.findByIdAndUpdate(id, planBody, {
-      new: true,
-      runValidators: true,
-    });
+    // If no matching meal found, push a new one
+    if (!updatedPlan) {
+      const plan = await Plans.findByIdAndUpdate(
+        id,
+        {
+          updatedBy: user._id,
+          $push: { meals: body },
+        },
+        { new: true, runValidators: true }
+      );
+      return res.json({ data: plan });
+    }
 
-    res.json({ data: plan });
+    res.json({ data: updatedPlan });
   } catch (error) {
     next(error);
   }
--- routes/plans.js
+++ routes/plans.js
@@ -1,16 +1,10 @@
 import express from "express";
-import {
-  createMeal,
-  createPlan,
-  deletePlan,
-  getAllPlans,
-  getPlan,
-} from "../controllers/plans.js";
+import { createPlan, getAllPlans, getPlan, updateMeal } from "../controllers/plans.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllPlans).post(createPlan);
-router.route("/:id").get(getPlan).delete(deletePlan);
-router.route("/:id/meals").post(createMeal);
+router.route("/:id").get(getPlan);
+router.route("/:id/meals").post(updateMeal);
 
 export default router;
