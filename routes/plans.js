e7c8a2f95fcf47cd18f74aaadd972bf246f831e8
SakshiShreya <shreyasakshi96@gmail.com>
2026-03-25 01:52:28 +0530

    54: add delete function back

--- routes/plans.js
+++ routes/plans.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createPlan, getAllPlans, getPlan, updateMeal } from "../controllers/plans.js";
+import {
+  createPlan,
+  deletePlan,
+  getAllPlans,
+  getPlan,
+  updateMeal,
+} from "../controllers/plans.js";
 
 const router = express.Router();
 
 router.route("/").get(getAllPlans).post(createPlan);
-router.route("/:id").get(getPlan);
+router.route("/:id").get(getPlan).delete(deletePlan);
 router.route("/:id/meals").post(updateMeal);
 
 export default router;
