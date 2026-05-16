c003dee29df4a573b10ad318380cc98030c1c37f
Van-sh <72124535+Van-sh@users.noreply.github.com>
2026-05-17 00:46:03 +0530

    implement start plan

--- controllers/plans.js
+++ controllers/plans.js
@@ -1,14 +1,14 @@
 import Plans from "../models/plans.js";
+import Users from "../models/users.js";
 
 export async function getAllPlans(req, res, next) {
   try {
     const { page = 1, limit = 10, q = "" } = req.query;
     const filter = { name: { $regex: q, $options: "i" } };
-    const findPromise = Plans.find(
-      filter,
-      {},
-      { skip: limit * (page - 1), limit },
-    ).populate("updatedBy", "name fName lName");
+    const findPromise = Plans.find(filter, {}, { skip: limit * (page - 1), limit }).populate(
+      "updatedBy",
+      "name fName lName",
+    );
     const countPromise = Plans.countDocuments(filter);
     const [plans, count] = await Promise.all([findPromise, countPromise]);
     res.json({ data: plans, count });
@@ -63,6 +63,56 @@ export async function deletePlan(req, res, next) {
     await Plans.findByIdAndDelete(id);
 
     res.status(204).json();
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function startPlan(req, res, next) {
+  try {
+    const { id } = req.params;
+    const { weeks } = req.body;
+    const { user } = req;
+
+    if (weeks == null) {
+      throw { statusCode: 400, message: "Plan duration (weeks) is required" };
+    }
+
+    if (!Number.isInteger(weeks)) {
+      throw { statusCode: 400, message: "Plan duration must be a whole number of weeks" };
+    }
+
+    const plan = await Plans.findById(id).select("_id name isPrivate createdBy");
+    if (!plan) {
+      throw { statusCode: 404, message: "Plan not found" };
+    }
+
+    if (plan.isPrivate && plan.createdBy.toString() !== user._id.toString()) {
+      throw { statusCode: 403, message: "You don't have permission to start this plan" };
+    }
+
+    const startedAt = new Date();
+    const endsAt = new Date(startedAt.getTime() + weeks * 7 * 24 * 60 * 60 * 1000);
+
+    await Users.findByIdAndUpdate(
+      user._id,
+      {
+        currentPlan: {
+          plan: plan._id,
+          weeks,
+          startedAt,
+          endsAt,
+        },
+      },
+      { new: true, runValidators: true },
+    );
+
+    const updatedUser = await Users.findById(user._id).populate("currentPlan.plan", "name");
+    const userData = updatedUser.toJSON();
+    delete userData.password;
+    delete userData.__v;
+
+    res.json({ data: userData });
   } catch (error) {
     next(error);
   }
--- routes/plans.js
+++ routes/plans.js
@@ -5,6 +5,7 @@ import {
   deletePlan,
   getAllPlans,
   getPlan,
+  startPlan,
   updateMeal,
 } from "../controllers/plans.js";
 
@@ -14,5 +15,6 @@ router.route("/").get(getAllPlans).post(createPlan);
 router.route("/:id").get(getPlan).delete(deletePlan);
 router.route("/:id/meals").post(updateMeal);
 router.route("/:id/meals/:mealId").delete(deleteMeal);
+router.route("/:id/start").post(startPlan);
 
 export default router;
